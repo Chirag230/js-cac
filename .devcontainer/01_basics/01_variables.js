f7e856528485494f35aebaebc651e01335c86928
Chirag Garg <108383033+Chirag230@users.noreply.github.com>
2023-11-08 07:12:44 +0000

    datatypes

---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 144553
+let accountEmail  ="chirag@google.com"
+var accountPassword = "12345"
+accountCity="Haryana"
+
+// accountId = 2
+console.log(accountId);
+accountEmail =  "yewuryq.com"
+accountPassword=22412
+accountCity="Punjab"
+/* prefer not to use var*/
+console.table([accountId,accountEmail,accountPassword,accountCity])
---
+++ .devcontainer/01_basics/02_variable.js
@@ -0,0 +1,11 @@
+"use strict"; // treat all Js code as newer version
+
+//alert(3+3)// we using nodejs not browser
+
+console.log(3+3)
+let age=18
+let isLoggedIn=true
+let state=null
+console.log(typeof age)
+console.log(typeof null)//object
+console.log(typeof undefined)//undefined
