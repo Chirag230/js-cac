668ef58ed1d2142a27a0ab464a7fc1d9c3e3d282
Chirag Garg <108383033+Chirag230@users.noreply.github.com>
2023-11-10 14:31:36 +0000

    functions

---
+++ .devcontainer/functions.js
@@ -0,0 +1,19 @@
+function sayMyName(){
+    console.log("C");
+    console.log("H");
+    console.log("I");
+    console.log("R");
+    console.log("A");
+    console.log("G");
+}
+// sayMyName()
+
+function addTwoNumbers(number1,number2){
+    return number1 + number2
+};
+let result = addTwoNumbers(3,4)
+console.log(result);
+addTwoNumbers(3,"4")
+addTwoNumbers("3",4)
+addTwoNumbers(3,"a")
+addTwoNumbers(3,null)
