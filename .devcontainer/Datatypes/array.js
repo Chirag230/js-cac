800268a2d46ab88c99861b47190890789e6d1b30
Chirag Garg <108383033+Chirag230@users.noreply.github.com>
2023-11-10 08:44:08 +0000

    arrays

--- .devcontainer/Datatypes/array.js
+++ .devcontainer/Datatypes/array.js
@@ -1,5 +1,52 @@
-const myArr = [0,1,2,3,4,5,true,"Chirag"]
-console.log(myArr[6]);
+//const myArr = [0,1,2,3,4,5,true,"Chirag"]
+// console.log(myArr[6]);
 
-const myHeroes = ["barny","joeyy"]
-const myarr2 = new Array(1,2,3,4)
+// const myHeroes = ["barny","joeyy"]
+// const myarr2 = new Array(1,2,3,4)
+
+const myArr = [1,2,3,4,5]
+// myArr.push(6)
+// console.log(myArr);
+// myArr.unshift(9)
+// console.log(myArr);
+// myArr.shift()
+// console.log(myArr);
+// console.log(myArr.includes(9));
+// console.log(myArr.indexOf(9));
+// const newArr = myArr.join()
+// console.log(newArr);
+// console.log(typeof newArr);
+
+// const myn1   =  myArr.slice(1,3);
+// console.log(myn1)
+// console.log("A:" ,myArr);
+
+// const myn2 = myArr.splice(1,3);
+// console.log(myn2);
+// console.log("B:",myArr);
+
+const marvel = ["thor","ironman","spiderman"]
+const dc = ["superman","flash","batman"]
+
+// marvel.push(dc)
+// console.log(marvel);
+// console.log(marvel[3][0]);
+// const all  = marvel.concat(dc)
+// console.log(all);
+
+// const all_hero = [...marvel,...dc]//spread 
+// console.log(all_hero);
+
+const another_arr = [1,2,3,[4,5,6],7,[8,9,[3,4]]]
+const newa = another_arr.flat(Infinity)
+console.log(newa);
+
+console.log(Array.from("CHIRAG  "))
+console.log(Array.from({name:"Chirag"}));//empty array because we didn't specify of to whose array key or value
+
+let score1 = 100
+let score2 = 200
+let score3 = 300
+
+console.log(Array.of(score1,score2,score3));
+
