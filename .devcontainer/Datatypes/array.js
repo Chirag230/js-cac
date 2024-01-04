bf36679ccbb2bfee22c91960045cede2fa292ea4
Chirag Garg <108383033+Chirag230@users.noreply.github.com>
2024-01-04 08:15:10 +0000

    array

--- .devcontainer/Datatypes/array.js
+++ .devcontainer/Datatypes/array.js
@@ -1,18 +1,19 @@
-//const myArr = [0,1,2,3,4,5,true,"Chirag"]
+const myArr = [0,1,2,3,4,5,true,"Chirag"]
+console.log(typeof newArr);
 // console.log(myArr[6]);
 
 // const myHeroes = ["barny","joeyy"]
 // const myarr2 = new Array(1,2,3,4)
 
-const myArr = [1,2,3,4,5]
+// const myArr = [1,2,3,4,5]
 // myArr.push(6)
 // console.log(myArr);
 // myArr.unshift(9)
 // console.log(myArr);
 // myArr.shift()
-// console.log(myArr);
 // console.log(myArr.includes(9));
-// console.log(myArr.indexOf(9));
+// console.log(myArr);
+// console.log(myArr.indexOf(9));a
 // const newArr = myArr.join()
 // console.log(newArr);
 // console.log(typeof newArr);
@@ -25,8 +26,8 @@ const myArr = [1,2,3,4,5]
 // console.log(myn2);
 // console.log("B:",myArr);
 
-const marvel = ["thor","ironman","spiderman"]
-const dc = ["superman","flash","batman"]
+// const marvel = ["thor","ironman","spiderman"]
+// const dc = ["superman","flash","batman"]
 
 // marvel.push(dc)
 // console.log(marvel);
@@ -37,16 +38,16 @@ const dc = ["superman","flash","batman"]
 // const all_hero = [...marvel,...dc]//spread 
 // console.log(all_hero);
 
-const another_arr = [1,2,3,[4,5,6],7,[8,9,[3,4]]]
-const newa = another_arr.flat(Infinity)
-console.log(newa);
+// const another_arr = [1,2,3,[4,5,6],7,[8,9,[3,4]]]
+// const newa = another_arr.flat(Infinity)
+// console.log(newa);
 
-console.log(Array.from("CHIRAG  "))
-console.log(Array.from({name:"Chirag"}));//empty array because we didn't specify of to whose array key or value
+// console.log(Array.from("CHIRAG  "))
+// console.log(Array.from({name:"Chirag"}));//empty array because we didn't specify of to whose array key or value
 
-let score1 = 100
-let score2 = 200
-let score3 = 300
+// let score1 = 100
+// let score2 = 200
+// let score3 = 300
 
-console.log(Array.of(score1,score2,score3));
+// console.log(Array.of(score1,score2,score3));
 
